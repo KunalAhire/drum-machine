28fff09002037880f51add0fe6e1699c47783405
KunalAhire <ahirekunal.3011@gmail.com>
2023-11-23 15:25:56 +0530

    bug fixing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import './App.css';
 import DrumPads from './components/DrumPads';
 import DrumTools from './components/DrumTools';
